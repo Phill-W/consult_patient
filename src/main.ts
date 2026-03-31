1f16c5357c505d5e690c22b203f0cd16f2287de4
Phil <1453139442@qq.com>
2026-03-31 21:08:26 +0800

    feat:仓库统一导出

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import persist from 'pinia-plugin-persistedstate'
+import pinia from '@/stores'
 
-// 在main.scss之前引入，后面需要去覆盖vant的样式
 import 'vant/lib/index.css'
 import './styles/main.scss'
 
 const app = createApp(App)
 
-app.use(createPinia().use(persist))
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
