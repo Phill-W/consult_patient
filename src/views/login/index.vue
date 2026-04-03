aabb8143a8ad1867abbbbf4eda82ee69e7f18d8c
Phil <1453139442@qq.com>
2026-04-03 14:28:08 +0800

    feat: cp-nav-bai 功能标题和右侧文字以及右侧文字注册点击事件

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 // import { Button as VanButton, Checkbox as VanCheckbox } from 'vant'
+const fn = () => {
+  console.log('点击了注册')
+}
 </script>
 <template>
   <div class="login-page">
-    <cp-nav-bar></cp-nav-bar>
+    <cp-nav-bar title="登录" right-text="注册" @click-right="fn"></cp-nav-bar>
     <van-button>按钮</van-button>
     <van-checkbox>复选框</van-checkbox>
   </div>
