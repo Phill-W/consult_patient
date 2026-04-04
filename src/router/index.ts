1ec29d75613abc1764dba0f753e98243e9f70b2b
Phil <1453139442@qq.com>
2026-04-04 18:02:53 +0800

    feat: 全局后置设置标题

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,23 +6,34 @@ const router = createRouter({
   routes: [
     {
       path: '/login',
-      component: () => import('@/views/login/index.vue')
+      component: () => import('@/views/login/index.vue'),
+      meta: { title: '登录' }
     },
     {
       path: '/',
       redirect: '/home',
       component: () => import('@/views/Layout/index.vue'),
       children: [
-        { path: '/home', component: () => import('@/views/Home/index.vue') },
+        {
+          path: '/home',
+          component: () => import('@/views/Home/index.vue'),
+          meta: { title: '首页' }
+        },
         {
           path: '/article',
-          component: () => import('@/views/Article/index.vue')
+          component: () => import('@/views/Article/index.vue'),
+          meta: { title: '健康百科' }
         },
         {
           path: '/notify',
-          component: () => import('@/views/Notify/index.vue')
+          component: () => import('@/views/Notify/index.vue'),
+          meta: { title: '消息通知' }
         },
-        { path: '/user', component: () => import('@/views/User/index.vue') }
+        {
+          path: '/user',
+          component: () => import('@/views/User/index.vue'),
+          meta: { title: '个人中心' }
+        }
       ]
     }
   ]
@@ -33,5 +44,10 @@ router.beforeEach((to) => {
   const store = useUserStore()
   const whiteList = ['/login']
   if (!store.user?.token && !whiteList.includes(to.path)) return '/login'
+})
+
+//全局的后置导航
+router.afterEach((to) => {
+  document.title = `${to.meta.title || ''}-优医问诊`
 })
 export default router
