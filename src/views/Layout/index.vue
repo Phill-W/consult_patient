102a53ed9debfb008b9bef897f09eb0a6676cb70
Phil <1453139442@qq.com>
2026-04-04 16:18:22 +0800

    feat: 布局容器与布局规则

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="layout-page">
+    <router-view></router-view>
+    <div>tabbar</div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
