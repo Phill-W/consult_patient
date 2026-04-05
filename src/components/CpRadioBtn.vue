02466e57113c8bb100f68984426bc51beb3ee0b4
Phil <1453139442@qq.com>
2026-04-05 18:01:17 +0800

    feat: 单选按钮组件封装

--- src/components/CpRadioBtn.vue
+++ src/components/CpRadioBtn.vue
@@ -1,41 +1,47 @@
-<!-- <script setup lang="ts">
-//计数器
-//通过 v-model 解析成modelValue @update:modelValue
-defineProps<{
-  modelValue: number
-}>()
-
-defineEmits<{ (e: 'update:modelValue', count: number): void }>()
-// const modelValue = defineModel<number>({ default: 0 })
-// function update() {
-//   modelValue.value++
-// }
-</script>
-
-<template>
-  <div class="cp-radio-btn">计数器：{{ modelValue }}</div>
-  <button @click="$emit('update:modelValue', modelValue + 1)">++</button>
-</template>
-
-<style lang="scss" scoped></style> -->
-
 <script setup lang="ts">
-//计数器
-//通过 v-model 解析成modelValue @update:modelValue
 defineProps<{
-  count: number
+  options: { label: string; value: number | string }[]
+  modelValue: number | string
 }>()
-
-defineEmits<{ (e: 'update:count', count: number): void }>()
-// const modelValue = defineModel<number>({ default: 0 })
-// function update() {
-//   modelValue.value++
-// }
+defineEmits<{ (e: 'update:modelValue', value: number | string): void }>()
 </script>
 
 <template>
-  <div class="cp-radio-btn">计数器：{{ count }}</div>
-  <button @click="$emit('update:count', count + 1)">++</button>
+  <div class="cp-radio-btn">
+    <a
+      class="item"
+      href="javascript:;"
+      v-for="item in options"
+      :key="item.value"
+      @click="$emit('update:modelValue', item.value)"
+      :class="{ active: item.value === modelValue }"
+      >{{ item.label }}</a
+    >
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.cp-radio-btn {
+  display: flex;
+  flex-wrap: wrap;
+  .item {
+    height: 32px;
+    min-width: 60px;
+    line-height: 30px;
+    padding: 0 14px;
+    text-align: center;
+    border: 1px solid var(--cp-bg);
+    background-color: var(--cp-bg);
+    margin-right: 10px;
+    box-sizing: border-box;
+    color: var(--cp-text2);
+    margin-bottom: 10px;
+    border-radius: 4px;
+    transition: all 0.3s;
+    &.active {
+      border-color: var(--cp-primary);
+      background-color: var(--cp-plain);
+    }
+  }
+}
+</style>
--- src/views/User/PatientPage.vue
+++ src/views/User/PatientPage.vue
@@ -10,8 +10,11 @@ const loadList = async () => {
 onMounted(() => {
   loadList()
 })
-
-const count = ref(10)
+const options = [
+  { label: '男', value: 1 },
+  { label: '女', value: 0 }
+]
+const gender = ref(1)
 </script>
 
 <template>
@@ -40,7 +43,7 @@ const count = ref(10)
         :model-value="count"
         @update:model-value="count = $event"
       ></cp-radio-btn> -->
-      <cp-radio-btn v-model:count="count"></cp-radio-btn>
+      <cp-radio-btn :options="options" v-model="gender"></cp-radio-btn>
     </div>
   </div>
 </template>
