6daf8004276154ee783bc5696ac40322995562e9
Phil <1453139442@qq.com>
2026-04-05 15:12:37 +0800

    feat: 个人中心-用户信息类型

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -19,3 +19,29 @@ type CodeType =
   | 'changeMobile'
   | 'forgetPassword'
   | 'bindMobile'
+
+// Pick 作用？
+// 从类型对象中取出指定的属性类型
+// Omit 作用？
+// 从类型对象中排出指定的属性类型，得到剩余的
+type OmitUser = Omit<User, 'token'>
+type UserInfo = OmitUser & {
+  /** 关注 */
+  likeNumber: number
+  /** 收藏 */
+  collectionNumber: number
+  /** 积分 */
+  score: number
+  /** 优惠券 */
+  couponNumber: number
+  orderInfo: {
+    /** 待付款 */
+    paidNumber: number
+    /** 待发货 */
+    receivedNumber: number
+    /** 待收货 */
+    shippedNumber: number
+    /** 已完成 */
+    finishedNumber: number
+  }
+}
