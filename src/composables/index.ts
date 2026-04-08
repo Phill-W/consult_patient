326c99830f7666a083f843c6142b73501a74c248
Phil <1453139442@qq.com>
2026-04-08 19:13:10 +0800

    feat: 首页模块-逻辑复用

---
+++ src/composables/index.ts
@@ -0,0 +1,18 @@
+import { followOrUnfollow } from '@/service/consult'
+import { ref } from 'vue'
+
+//vue3概念：组合式API，组合式函数（composable） useXxx
+//composable
+export const useFollow = (type: FollowType = 'doc') => {
+  const loading = ref(false)
+  const follow = async (item: { id: string; likeFlag: 0 | 1 }) => {
+    loading.value = true
+    try {
+      await followOrUnfollow(item.id, type)
+      item.likeFlag = item.likeFlag === 1 ? 0 : 1
+    } finally {
+      loading.value = false
+    }
+  }
+  return { loading, follow }
+}
--- src/views/Home/components/KnowledgeCard.vue
+++ src/views/Home/components/KnowledgeCard.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import { useFollow } from '@/composables'
 defineProps<{
   item: Knowledge
 }>()
+const { loading, follow } = useFollow('knowledge')
 </script>
 
 <template>
@@ -15,9 +17,14 @@ defineProps<{
           {{ item.creatorTitles }}
         </p>
       </div>
-      <van-button class="btn" size="small" round>{{
-        item.likeFlag ? '已关注' : '+关注'
-      }}</van-button>
+      <van-button
+        class="btn"
+        size="small"
+        round
+        :loading="loading"
+        @click="follow(item)"
+        >{{ item.likeFlag ? '已关注' : '+关注' }}</van-button
+      >
     </div>
     <div class="body">
       <h3 class="title van-ellipsis">
