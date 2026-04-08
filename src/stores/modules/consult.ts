46703cf56f2bec3a75f24d808815ba9ef7fe3537
Phil <1453139442@qq.com>
2026-04-08 20:18:28 +0800

    feat: 极速问诊-选择问诊类型

---
+++ src/stores/modules/consult.ts
@@ -0,0 +1,19 @@
+import type { PartialConsult } from '@/types/consult'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import type { ConsultType } from '@/enums'
+
+export const useConsultStore = defineStore(
+  'cp-consult',
+  () => {
+    //问诊信息
+    const consult = ref<PartialConsult>({})
+    // 记录问诊类型
+    const setType = (type: ConsultType) => (consult.value.type = type)
+
+    return { consult, setType }
+  },
+  {
+    persist: true
+  }
+)
