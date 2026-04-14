db69eedcae57f69215de5634fa6fd69769ab6540
Phil <1453139442@qq.com>
2026-04-14 16:32:47 +0800

    feat: 问诊记录-搭建页面

---
+++ src/views/User/components/ConsultList.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import ConsultItem from './ConsultItem.vue'
+import type { ConsultType } from '@/enums'
+defineProps<{
+  type: ConsultType
+}>()
+</script>
+
+<template>
+  <div class="consult-list">
+    <consult-item v-for="i in 5" :key="i" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.consult-list {
+  padding: 10px 15px;
+}
+</style>
